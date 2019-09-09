129c753994bf3b4e60ecbbafc1584b08e30003b9
laetitia123 <laetitiauw1@gmail.com>
2019-09-09 13:17:02 +0200

    changes

--- credentials_test.py
+++ credentials_test.py
@@ -1,6 +1,9 @@
  ## credentials test
 import unittest #importing the unittest module
-from credentials import Credential #importing the credentials class
+from credentials import Credential
+from user import User #importing the user class from user file
+
+
 
 
 class TestUser(unittest.TestCase):
@@ -38,7 +41,7 @@ class TestUser(unittest.TestCase):
         save_acct test case is to test if the account is being saved
         '''
         self.new_credential.save_credential()
-        self.assertEqual(len(Credential.credential_list),2)
+        self.assertEqual(len(Credential.credential_list),1)
         
     def test_display_credentials(self):
         '''display_credential test case that returns  list of credentials
@@ -54,5 +57,40 @@ class TestUser(unittest.TestCase):
         test_credential= Credential("Test","username", "123456","test@user.com")#new credential
         test_credential.save_credential()
         
+    def test_credential_exist(self):
+        '''
+        test to check if credential exist
+        '''
+        self.new_credential.save_credential()
+        test_credential= Credential("test","stacy","123678","test@gmail.com")
+        test_credential.save_credential()
+        
+        credential_exists = Credential.credential_exist("test@gmail.com")
+        
+        self.assertTrue(credential_exist) 
+        
+    def test_delete_credential(self):
+        '''
+        test to test if a credential has been removed
+        '''
+        self.new_credential.save_credential()
+        test_credential= Credential("Test","username", "123456","test@user.com")
+        test_credential.save_credential()
+        
+        self.new_credential.delete_credential()
+        self.assertEqual(len(Credential.credential_list),1)
+              
+import pyperclip
+
+    
+def test_copy_credential(self):
+        '''
+        test to check if credentials have been copied
+        '''
+        self.new_credential.save_credential()
+        Credential.copy_credential("test@user")
+        self.assertEqual(self.new_credential.email,pyperclip.paste())
+        
+            
 if __name__ == '__main__':
     unittest.main()        
